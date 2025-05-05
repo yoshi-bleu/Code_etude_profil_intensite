7713e2b323ac0feb10582bb5d423b60d36aca6d6
yoshi-bleu <al.lemestre@gmail.com>
2025-05-05 15:30:03 +0200

    Update assemblage.py

--- assemblage.py
+++ assemblage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from IMPORTS import *  # Importer tout depuis imports.py
 
+from coordonnees import *  # Importer tout depuis coordonnees.py
+
 from imageJ_v2 import * # Importer tout depuis imageJ_v2.py
 
 from Traitement import *  # Importer tout depuis Traitement.py
-
