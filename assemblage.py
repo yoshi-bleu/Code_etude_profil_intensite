7934829d5463e1e5c113ee18a5fb1722d6b97941
yoshi-bleu <al.lemestre@gmail.com>
2025-05-27 18:03:16 +0200

    Delete assemblage.py

--- assemblage.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from IMPORTS import *  # Importer tout depuis imports.py
-
-from coordonnees import *  # Importer tout depuis coordonnees.py
-
-from imageJ_v2 import * # Importer tout depuis imageJ_v2.py
-
-from Traitement import *  # Importer tout depuis Traitement.py
